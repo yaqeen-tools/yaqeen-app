db005a799a87476d05780410e1278564dfcd5f45
yaqeen-tools <support.yaqeen@gmail.com>
2026-09-17 14:03:32 +0400

    brand: add company info constants

---
+++ lib/companyInfo.ts
@@ -0,0 +1,8 @@
+export const COMPANY = {
+  nameAr: 'يقين',
+  nameEn: 'Yaqeen',
+  email: 'support.yaqeen@gmail.com',
+  phone: '+971 50 684 5592',
+  addressAr: 'دولة الإمارات العربية المتحدة',
+  addressEn: 'United Arab Emirates',
+};
