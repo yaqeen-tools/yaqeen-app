5b14369e88acefc31e419b1d9997c6f26363ee8b
yaqeen-tools <support.yaqeen@gmail.com>
2026-09-17 14:11:12 +0400

    brand: update legal name to Yaqeen Digital Solutions

--- lib/companyInfo.ts
+++ lib/companyInfo.ts
@@ -1,6 +1,8 @@
 export const COMPANY = {
   nameAr: 'يقين',
+  nameFullAr: 'يقين للحلول الرقمية',
   nameEn: 'Yaqeen',
+  nameFullEn: 'Yaqeen Digital Solutions',
   email: 'support.yaqeen@gmail.com',
   phone: '+971 50 684 5592',
   addressAr: 'دولة الإمارات العربية المتحدة',
